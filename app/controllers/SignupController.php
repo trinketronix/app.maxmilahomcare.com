92288e27c8ecbcc208c9a110bd859a6899c442f1
Edmundo Chavez <edmundo.chavez@gmail.com>
2024-12-27 22:35:49 -0600

    fixcontrollers

--- app/controllers/SignupController.php
+++ app/controllers/SignupController.php
@@ -1,9 +1,16 @@
 <?php
 
-use Phalcon\Http\Response;
-use Phalcon\Http\Client\Request;
+//use Phalcon\Http\Response;
+//use Phalcon\Http\Client\Request;
 
-class SignupController extends ControllerBase
+namespace Homecare\Controllers;
+
+use Phalcon\Http\Response;
+use Phalcon\Http\Request;
+use Phalcon\Mvc\Controller;
+
+
+class SignupController extends Controller
 {
     public function indexAction()
     {
