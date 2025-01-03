cbf607cb12de0bf7637f1e9efdffca97374340c5
Edmundo Chavez <edmundo.chavez@gmail.com>
2025-01-02 23:47:40 -0600

    fix hash in change password

--- app/controllers/ForgotController.php
+++ app/controllers/ForgotController.php
@@ -5,18 +5,20 @@
 
 namespace Homecare\Controllers;
 
+use Exception;
 use Phalcon\Http\Response;
 use Phalcon\Http\Request;
 use Phalcon\Mvc\Controller;
+use Homecare\Utils\HttpRequest;
 
 class ForgotController extends BaseController
 {
-    public function indexAction()
-    {
+//    public function indexAction()
+//    {
 //        if ($this->request->isPost()) {
 //            $username = $this->request->getPost('username');
 //            $newPassword = $this->request->getPost('new_password');
-//
+
 //            try {
 //                $request = new Request();
 //                $request->setBaseUri('https://api.maximus.io');
@@ -25,10 +27,10 @@ class ForgotController extends BaseController
 //                    'username' => $username,
 //                    'new_password' => $newPassword
 //                ]);
-//
+
 //                $response = $request->post('/change-password');
 //                $result = json_decode($response->getBody());
-//
+
 //                if ($result->success) {
 //                    $this->flash->success('Password changed successfully');
 //                    return $this->response->redirect('login');
@@ -37,5 +39,53 @@ class ForgotController extends BaseController
 //                $this->flash->error('Error changing password');
 //            }
 //        }
+//    }
+
+
+
+    public function indexAction() {
+
+        if ($this->request->isPost()) {
+            $username = $this->request->getPost('username');
+            $password = $this->request->getPost('new_password');
+          //  $token = null;
+            // Prepare the JSON body for the login request
+            $jsonBody = json_encode(
+                [
+                    "username" => $username,
+                    "password" => $password
+                ],
+                JSON_THROW_ON_ERROR | JSON_UNESCAPED_SLASHES
+            );
+
+            try {
+                $forgotRequest = HttpRequest::put('/change-password', $jsonBody);
+                //file_put_contents('response_log.txt', print_r($forgotRequest, true));
+
+                if (empty($forgotRequest['data'])) {
+                    $this->flashSession->error($forgotRequest['message']);
+                }
+
+                $data = $forgotRequest['data'];
+                
+                //$token = $data['token'];
+
+                // Set session data
+                //$this->session->set('auth-token', $token);
+                //$this->session->set('username', $username);
+
+                // Redirect to the 'main' page
+                //if ($token != null)
+                    $this->response->redirect('main');
+
+            } catch (Exception $e) {
+                // Handle errors in the API requests
+                $this->flashSession->error('An error occurred during the password reset: ' . $e->getMessage());
+                return $this->dispatcher->forward([
+                    'controller' => 'login',
+                    'action' => 'index'
+                ]);
+            }
+        }
     }
-} 
+}
