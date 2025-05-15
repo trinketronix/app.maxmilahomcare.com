e52d9c7606aa19d97a8c1da53ca69f5c75cbb984
Todd Salpen <toddsalpen@gmail.com>
2025-05-15 04:39:48 -0400

    refactor: files organizing

---
+++ app/bootstrap/router.php
@@ -0,0 +1,54 @@
+<?php
+
+/**
+ * Router configuration
+ * Extracts the router setup from index.php
+ */
+
+use Phalcon\Mvc\Router;
+
+// Setup the router
+if (isset($di)) {
+    $di->set('router', function () {
+        $router = new Router();
+
+        // Define routes map
+        $routes = [
+            '/' => ['controller' => 'login'],
+            '/login' => ['controller' => 'login'],
+            '/list' => ['controller' => 'list'],
+            '/forgot' => ['controller' => 'forgot'],
+            '/signup' => ['controller' => 'signup'],
+            '/main' => ['controller' => 'main'],
+            '/test' => ['controller' => 'test'],
+            '/details' => ['controller' => 'details'],
+            '/detailsusers' => ['controller' => 'detailsusers'],
+            '/logout' => ['controller' => 'logout'],
+            '/activate' => ['controller' => 'activate'],
+            '/changerole' => ['controller' => 'changerole'],
+            '/users' => ['controller' => 'users'],
+            '/user' => ['controller' => 'user'],
+            '/patients' => ['controller' => 'patients'],
+            '/userupdate' => ['controller' => 'userupdate'],
+            '/caregiver' => ['controller' => 'caregiver'],
+            '/visit' => ['controller' => 'visit'],
+        ];
+
+        // Register all routes
+        foreach ($routes as $pattern => $route) {
+            $router->add($pattern, array_merge([
+                'namespace' => 'Homecare\Controllers',
+                'action' => 'index'
+            ], $route));
+        }
+
+        // Set the 404 not found page
+        $router->notFound([
+            'namespace'  => 'Homecare\Controllers',
+            'controller' => 'login',
+            'action'     => 'index'
+        ]);
+
+        return $router;
+    });
+}
