12baf2e05f396091e41f2db528249b588f37f40f
Edmundo Chavez <edmundo.chavez@gmail.com>
2025-05-21 06:16:32 -0600

    prepare for assigning patients

---
+++ app/controllers/AssignPatient.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Homecare\controllers;
+use Homecare\Utils\Endpoint;
+use Homecare\Utils\HttpRequest;
+class AssignPatient extends BaseController
+{
+    public function indexAction()
+    {
+        $token = $this->session->get('auth-token');
+        $headers = ["Authorization" => $token];
+      //  require_once 'c:\xampp\htdocs\app.maxmila.com\app\views\users\index.php';
+        if ($token != null) {
+            $response = HttpRequest::get(Endpoint::PATIENTS, $headers,[]);
+            if (!isset($response['data']['users'])) {
+                // Handle the case where 'accounts' is missing or not an array
+                $this->view->setVars([
+                    'accounts' => [],
+                    'error' => 'No se pudieron obtener los pacientes correctamente.',
+                ]);
+            } else {
+                $accounts = $response['data']['users'];
+            //    $this->view->setVar("selaccounts", $accounts);
+                $this->view->setVars([
+                  //  'id' => $accounts['id'],
+                    'selaccounts' => $accounts,
+                    'token' => $token,
+                    'baseUrl' => getenv('BASE_URL_API')?:'https://api-test.maxmilahomecare.com' ,
+                ]);
+
+            }
+        }
+    }
+}
